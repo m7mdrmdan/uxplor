eec5d3c4991f6908706319ee24afc2127a5e8dee
shrouk <shroukatefali@gmail.com>
2021-11-30 02:35:10 +0200

    updates

--- src/app/feature-modules/home/home.module.ts
+++ src/app/feature-modules/home/home.module.ts
@@ -6,13 +6,17 @@ import { HomeComponent } from './home/home.component';
 import { CardActionComponent } from './card-action/card-action.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AboutPetmanComponent } from './about-petman/about-petman.component';
+import { GallaryComponent } from './gallary/gallary.component';
+import { BrandsComponent } from './brands/brands.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     CardActionComponent,
-    AboutPetmanComponent
+    AboutPetmanComponent,
+    GallaryComponent,
+    BrandsComponent
   ],
   imports: [
     CommonModule,
